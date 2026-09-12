e5dbcec059c9c5620970244703aecabf03e04843
harsh_rana_dev <hr2322808@gmail.com>
2026-09-12 21:54:04 +0530

    brake

--- recap/main.py
+++
@@ -1,4 +0,0 @@
-orders_df[order_id, customer_id, order_date, amount, status]
-customers_df[customer_id, name, country, signup_date]
-import pandas as pd 
-import numpy as np
--- recap/main.sql
+++
@@ -1,3 +0,0 @@
-orders(order_id, customer_id, order_date, amount, status)
-customers(customer_id, name, country, signup_date)
-
