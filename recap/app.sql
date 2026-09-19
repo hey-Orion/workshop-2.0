a6dd29c80be2ee47fcf78fa3905c85fad0fe78cd
harsh_rana_dev <hr2322808@gmail.com>
2026-09-19 23:18:11 +0530

    done

--- recap/app.sql
+++ recap/app.sql
@@ -84,3 +84,87 @@ select c.name
 from customers c 
 left join orders o on c.customer_id = o.customer_id
 where o.order_id is NULL;
+
+
+
+select
+    order_id,
+    customer_id,
+    order_date,
+    amount,
+    lag(amount) over (
+        partition by customer_id 
+        order by order_date asc, order_id asc
+    ) as previous_order_amount
+from orders;
+
+with customer_aggregates as (
+    select  
+        c.customer_id,
+        c.name,
+        sum(o.amount) as total_spend
+    from customers c 
+    join orders o on c.customer_id = o.customer_id
+    group by c.customer_id, c.name
+)
+select
+    name,
+    total_spend,
+    dense_rank() over (
+        order by total_spend desc, customer_id asc 
+    ) as customer_rank
+from customer_aggregates;
+
+select
+    order_id,
+    customer_id,
+    amount,
+    avg(amount) over (
+        partition by customer_id
+    ) as customer_avg_amount,
+    amount - avg(amount) over (
+        partition by customer_id
+    ) as amount_diff
+from orders;
+
+with customer_totals as (
+    select  
+        c.customer_id,
+        c.name,
+        sum(o.amount) as total_amount
+    from customer c 
+    join orders on c.customer_id = o.customer_id 
+    group by c.customer_id, c.name 
+),
+global_avg_customer_spend as (
+    select 
+        avg(total_amount) as avg_customer_spend
+    from customer_totals
+)
+SELECT #
+    ct.customer_id,
+    ct.name,
+    ct.total_amount
+FROM customer_totals ct
+CROSS JOIN global_avg_customer_spend g
+WHERE ct.total_amount > g.avg_customer_spend;
+
+with ranked_orders as (
+    select
+        customer_id,
+        order_id,
+        order_date,
+        amount,
+        dense_rank() over (
+            partition by customer_id
+            order by amount desc, order_id asc 
+        ) as rnk 
+    from orders 
+)
+select
+    customer_id,
+    order_id,
+    order_date,
+    amount
+from ranked_orders
+where rnk = 2;
